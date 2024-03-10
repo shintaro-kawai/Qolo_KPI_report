e9a3f8facad46f0ce8b24d304271a054e2cdf1c8
shintaro <kawaishinta@gmail.com>
2024-03-10 12:19:05 +0900

    Make core folders and files

---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import time
+import matplotlib.pyplot as plt
+import streamlit as st
+import seaborn
+import plotly.express as px
+
+# from views.for_qolo import qolo_result
+# from views.for_hospital import hospital_result
